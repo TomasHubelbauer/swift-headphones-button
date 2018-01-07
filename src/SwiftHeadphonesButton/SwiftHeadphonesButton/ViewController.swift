5f11ebdb0bc5484aa9cf477b53c298b36f6c6db6
Tomas Hubelbauer <Tom@Tomass-MacBook-Air.local>
2018-01-07 13:38:23 +0100

    Scaffold a single view app Swift XCode project

---
+++ src/SwiftHeadphonesButton/SwiftHeadphonesButton/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftHeadphonesButton
+//
+//  Created by Tomas Hubelbauer on 07/01/2018.
+//  Copyright © 2018 Rhaeo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
